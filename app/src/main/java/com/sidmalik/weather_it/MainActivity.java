24cc29cc4e8642fa9af8414ff039cd10b21a79f4
Siddharth Malik <sidmalik123@icloud.com>
2017-01-09 18:10:36 -0500

    added a no connection dialog

--- app/src/main/java/com/sidmalik/weather_it/MainActivity.java
+++ app/src/main/java/com/sidmalik/weather_it/MainActivity.java
@@ -153,6 +153,8 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
+        }else{
+            alertUserAboutError();
         }
 
     }
