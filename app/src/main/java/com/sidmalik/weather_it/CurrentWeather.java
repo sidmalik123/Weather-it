5382d54e49d096420f254439e5a365083564a06a
Siddharth Malik <siddharthmalik123@gmail.com>
2017-01-10 20:40:40 -0500

    created parent class Weather, current derives from it

--- app/src/main/java/com/sidmalik/weather_it/CurrentWeather.java
+++ app/src/main/java/com/sidmalik/weather_it/CurrentWeather.java
@@ -16,85 +16,12 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
-public class CurrentWeather {
+public class CurrentWeather extends Weather {
     private static final String TAG = CurrentWeather.class.getSimpleName();
-    private String mIcon;
-    private long mTime;
-    private int mTemp;
     private double mHumidity;
     private  double mPrecip;
-    private String mSummary;
-    private String mTimezone;
 
 
-    public String getIcon() {
-        return mIcon;
-    }
-
-    public void setIcon(String icon) {
-        mIcon = icon;
-    }
-
-    public int getIconId(){
-        int iconId = -1;
-        if(mIcon.equals("clear-day")) {
-            iconId = R.drawable.clear_day;
-        }
-        else if (mIcon.equals("clear-night")) {
-            iconId = R.drawable.clear_night;
-        }
-        else if (mIcon.equals("rain")) {
-            iconId = R.drawable.rain;
-        }
-        else if (mIcon.equals("snow")) {
-            iconId = R.drawable.snow;
-        }
-        else if (mIcon.equals("sleet")) {
-            iconId = R.drawable.sleet;
-        }
-        else if (mIcon.equals("wind")) {
-            iconId = R.drawable.wind;
-        }
-        else if (mIcon.equals("fog")) {
-            iconId = R.drawable.fog;
-        }
-        else if (mIcon.equals("cloudy")) {
-            iconId = R.drawable.cloudy;
-        }
-        else if (mIcon.equals("partly-cloudy-day")) {
-            iconId = R.drawable.partly_cloudy;
-        }
-        else if (mIcon.equals("partly-cloudy-night")) {
-            iconId = R.drawable.cloudy_night;
-        }
-
-        return iconId;
-    }
-
-    public long getTime() {
-        return mTime;
-    }
-
-    public String getFormattedTime(){
-        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
-        formatter.setTimeZone(TimeZone.getTimeZone(getTimezone()));
-        Date dateTime = new Date(getTime() * 1000);
-        return formatter.format(dateTime);
-
-    }
-
-    public void setTime(long time) {
-        mTime = time;
-    }
-
-    public int getTemp() {
-        int celciusTemp = ((mTemp -32)*5)/9;
-        return celciusTemp;
-    }
-
-    public void setTemp(int temp) {
-        mTemp = temp;
-    }
 
     public double getHumidity() {
         return mHumidity;
@@ -112,23 +39,4 @@ public class CurrentWeather {
         mPrecip = precip;
     }
 
-    public String getSummary() {
-        return mSummary;
-    }
-
-    public void setSummary(String summary) {
-        mSummary = summary;
-    }
-
-    public String getTimezone() {
-        return mTimezone;
-    }
-
-    public void setTimezone(String timezone) {
-        mTimezone = timezone;
-    }
-
-    public String getStringObject() {
-        return "[ temperature: " + getTemp() + "]";
-    }
 }
---
+++ app/src/main/java/com/sidmalik/weather_it/Weather.java
@@ -0,0 +1,106 @@
+package com.sidmalik.weather_it;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Weather {
+    private long mTime;
+    private String mIcon;
+    private double mTemp;
+    private String mSummary;
+    private String mTimezone;
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+
+
+    public void setTemp(double temp) {
+        mTemp = temp;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
+    public String getTimezone() {
+        return mTimezone;
+    }
+
+    public void setTimezone(String timezone) {
+        mTimezone = timezone;
+    }
+
+    // methods with added functionalities
+    //
+    public int getIconId(){
+        int iconId = -1;
+        if(mIcon.equals("clear-day")) {
+            iconId = R.drawable.clear_day;
+        }
+        else if (mIcon.equals("clear-night")) {
+            iconId = R.drawable.clear_night;
+        }
+        else if (mIcon.equals("rain")) {
+            iconId = R.drawable.rain;
+        }
+        else if (mIcon.equals("snow")) {
+            iconId = R.drawable.snow;
+        }
+        else if (mIcon.equals("sleet")) {
+            iconId = R.drawable.sleet;
+        }
+        else if (mIcon.equals("wind")) {
+            iconId = R.drawable.wind;
+        }
+        else if (mIcon.equals("fog")) {
+            iconId = R.drawable.fog;
+        }
+        else if (mIcon.equals("cloudy")) {
+            iconId = R.drawable.cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-day")) {
+            iconId = R.drawable.partly_cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-night")) {
+            iconId = R.drawable.cloudy_night;
+        }
+
+        return iconId;
+    }
+
+
+
+    public String getFormattedTime(){
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(getTimezone()));
+        Date dateTime = new Date(getTime() * 1000);
+        return formatter.format(dateTime);
+
+    }
+
+
+    // no default getTemp, temp is always returned in celius
+    public int getTemp() {
+        int celciusTemp = (int) ((mTemp -32)*5)/9;
+        return celciusTemp;
+    }
+}
